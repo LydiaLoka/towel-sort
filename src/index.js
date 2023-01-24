328e9dc3c87954e9dea3b650583de00ba15ca3c2
LydiaLoka <sevmek87@gmail.com>
2023-01-24 17:40:16 +0300

    fix: resolve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let arrTranspond = [];
+  for(i=0; i<matrix.length; i++){
+  if (i % 2 != 0) {
+      arrTranspond.push(matrix[i].reverse());
+  } else {
+      arrTranspond.push(matrix[i]);
+  };
+  };    
+return arrTranspond.flat();
+
+  }
